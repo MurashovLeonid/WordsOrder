147a2fc29d3cf13ea0cb3ff9bdd7074bddd14878
Леонид Мурашов <lenja-d@yandex.ru>
2021-10-03 19:49:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace ConsoleGeneralTrain
+{
+    class ArrayAnalyser
+    {
+        public static string Order(string words)
+        {
+           return !words.Any()? String.Empty : 
+                String.Join(" ", words.Split(' ').OrderBy(i => i.Where(z => !Char.IsLetter(z)).Select(g => g.ToString()).ToArray().First()));
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "strin5g 3a 2is Thi1s te4st";
+            Console.WriteLine(ArrayAnalyser.Order(a));
+        }
+    }
+}
+
+
